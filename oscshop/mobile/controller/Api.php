f7fab717f8760276b6175c3feb29aa7d126b6a1b
s1223s <qin@gxwdbg.com>
2018-12-04 14:54:08 +0800

    更新券码核销接口

--- oscshop/mobile/controller/Api.php
+++ oscshop/mobile/controller/Api.php
@@ -21,19 +21,18 @@ class Api extends MobileBase{
 		//核销券码
 		function partnerwrite(){
 		$data = input('post.');
-		// print_r($data);
-				$fi='log.txt';
-		//$post = file_get_contents("php://input");
+		$fi='log.txt';
 		$sj = date('YmdHis').'<br>';
-		// $data = $sj.$data;
-		//file_put_contents($fi,$data,FILE_APPEND);
-		//file_put_contents($fi,$post,FILE_APPEND);
 		$msg_crrltn_id = $data['msg_crrltn_id'];
+		$msg_txn_code = $data['msg_txn_code'];
+		$coupon_no = $data['coupon_no'];
 		$msg_time = date("YmdHis");//时间
 		$msg_sys_sn = $data['msg_sys_sn'];//平台流水号
-		//$dataf = json_encode(array('msg_type'=>'01','msg_txn_code'=>'012100','msg_crrltn_id'=>$msg_crrltn_id,'msg_flg'=>'1','msg_sender'=>'660053','msg_time'=>$msg_time,'msg_sys_sn'=>$msg_sys_sn,'msg_ver'=>'0.1','msg_rsp_code'=>'0000','msg_rsp_desc'=>'success'));
-		//{"msg_type":"01","msg_txn_code":"002100","msg_crrltn_id":"".$msg_crrltn_id."","msg_flg":"1","msg_sender":"660053","msg_time":"".$msg_time."","msg_sys_sn":"".$msg_sys_sn."","msg_rsp_code":"0000","msg_rsp_desc":"成功"};
-		echo $dataf = 'msg_type=01&msg_txn_code=012100&msg_crrltn_id='.$msg_crrltn_id.'&msg_flg=1&msg_sender=660053&msg_time='.$msg_time.'&msg_sys_sn='.$msg_sys_sn.'msg_ver=0.1&msg_rsp_code=0000&msg_rsp_desc=success'.'</br>';
+
+		if ($msg_txn_code == '012100') {
+			Db::('coupon')->where('coupon_no',$coupon_no)->update(['creation_date' => $msg_time,'creation'=>'1']);
+		}
+		$dataf = 'msg_type=01&msg_txn_code='.$msg_txn_code.'&msg_crrltn_id='.$msg_crrltn_id.'&msg_flg=1&msg_sender=660053&msg_time='.$msg_time.'&msg_sys_sn='.$msg_sys_sn.'msg_ver=0.1&msg_rsp_code=0000&msg_rsp_desc=success';
 		$a = osc_sign();
 		$sign = $a ->rsaSign($dataf,file_get_contents('private.txt'));
 		$dataf = json_encode(array('msg_type'=>'01','msg_txn_code'=>'012100','msg_crrltn_id'=>$msg_crrltn_id,'msg_flg'=>'1','msg_sender'=>'660053','msg_time'=>$msg_time,'msg_sys_sn'=>$msg_sys_sn,'msg_ver'=>'0.1','msg_rsp_code'=>'0000','msg_rsp_desc'=>'success','sign'=>$sign));
